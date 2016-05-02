8bb5085f603aec0a8f4b787c7b0caa7fe26315a3
Konstantinos Konstantinidis <konstantinidis.kostas@gmail.com>
2016-05-02 13:21:03 +0100

    Adding santa deliveries problem

--- ruby/lib/santa_deliveries.rb
+++ ruby/lib/santa_deliveries.rb
@@ -1,14 +1,39 @@
 class SantaDeliveries
+  Point = Struct.new(:x, :y)
 
 	def initialize()
-		#@currentLocation = Struct.new(:x, :y)
+    @deliveries = Hash.new
+    @currentLocation = Point.new(0, 0)
 	end
 
 	def count_houses
-
+    @deliveries.count
 	end
 
 	def deliver_to(direction)
+    case direction
+      when '>'
+        @currentLocation = Point.new(@currentLocation.x + 1,
+          @currentLocation.y)
+      when 'v'
+        @currentLocation = Point.new(@currentLocation.x,
+          @currentLocation.y - 1)
+      when '^'
+        @currentLocation = Point.new(@currentLocation.x,
+          @currentLocation.y + 1)
+      when '<'
+        @currentLocation = Point.new(@currentLocation.x - 1,
+          @currentLocation.y)
+      else
+        raise "bad input #{direction}"
+      end
 
+      locationKey = "#{@currentLocation.x},#{@currentLocation.y}"
+      puts locationKey
+      if(@deliveries[locationKey])
+        @deliveries[locationKey] = @deliveries[locationKey] + 1
+      else
+        @deliveries[locationKey] = 1
+      end
 	end
 end
--- ruby/spec/santa_deliveries_spec.rb
+++ ruby/spec/santa_deliveries_spec.rb
@@ -1,16 +1,59 @@
 require 'santa_deliveries'
 
-RSpec.describe SantaDeliveries do
-	it 'should deliver to none' do
-		assert 0, @santa_deliveries.count_houses
+RSpec.describe SantaDeliveries, "when santa starts the present deliveries" do
+	let(:deliveries) { @santa_deliveries.count_houses}
+
+	before do
+		@santa_deliveries = SantaDeliveries.new
 	end
 
-	context 'when navigating to > house' do
+	it 'should deliver to none' do
+		expect(deliveries).to eq(0)
+	end
+
+	context 'when navigating to a house' do
 		before do
 			@santa_deliveries.deliver_to('>')
 		end
 
-		it 'should return 1' do
+		it "should deliver one house" do
+			expect(deliveries).to eq(1)
+		end
+	end
+
+	context "when navigating to two houses" do
+		before do 
+			'>>'.each_char do |x| 
+				@santa_deliveries.deliver_to(x)
+			end
+		end
+
+		it "should deliver to two" do
+			expect(deliveries).to eq(2)
+		end
+	end
+
+	context "when navigating to the same house twice" do
+		before do 
+			'>><'.each_char do |x| 
+				@santa_deliveries.deliver_to(x)
+			end
+		end
+
+		it "should deliver to two unique houses" do
+			expect(deliveries).to eq(2)
+		end
+	end
+
+	context "when navigating to all directions" do
+		before do 
+			'><^<v'.each_char do |x| 
+				@santa_deliveries.deliver_to(x)
+			end
+		end
+
+		it "should deliver to all houses" do
+			expect(deliveries).to eq(5)
 		end
 	end
 end
